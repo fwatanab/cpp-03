a1b3264a9240b7ff5f292562786165c557d1b9d0
fwatanab <fwatanab@student.42tokyo.jp>
2024-04-26 12:14:05 +0900

    del status / デバック用

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,15 +5,11 @@ int	main() {
 
 	claptrap1.attack("CT2");
 	claptrap2.takeDamage(3);
-	claptrap2.status();
 
 	claptrap2.beRepaired(5);
-	claptrap2.status();
 
 	claptrap1.attack("CT2");
 	claptrap2.takeDamage(2);
-	claptrap1.status();
-	claptrap2.status();
 
 	return 0;
 }

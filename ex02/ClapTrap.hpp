9c3bdef444d1666ed1628338190753b778002738
fwatanab <fwatanab@student.42tokyo.jp>
2024-04-26 09:39:39 +0900

    correction of  prompt messages

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class	ClapTrap {
+	protected:
+		std::string	name;
+		int			hitPoint;
+		int			energyPoint;
+		int			attackDamage;
+	public:
+		ClapTrap(std::string name);
+		virtual ~ClapTrap();
+		virtual void	attack(const std::string& target);
+		void			takeDamage(unsigned int amount);
+		void			beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ ex02/ScavTrap.cpp
@@ -0,0 +1,25 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+	hitPoint = 100;
+	energyPoint = 50;
+	attackDamage = 20;
+	std::cout << "ScavTrap " << name << "Inheritance constructor" << std::endl;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap " << name << "Inheritance destructor" << std::endl;
+}
+
+void	ScavTrap::attack(const std::string& target) {
+	if (energyPoint > 0) {
+		energyPoint--;
+		std::cout << "ScavTrap " << name << " savagely attacks " << target << ", causing " << attackDamage << " points of damage!" << std::endl;
+	}
+	else
+		std::cerr << "Error: NOT enough energy points" << std::endl;
+}
+
+void	ScavTrap::guardGate() {
+	std::cout << "ScavTrap " << name << " is now in Gate Keeper mode." << std::endl;
+}

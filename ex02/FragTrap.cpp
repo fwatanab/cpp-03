56328063c6d5532fbcad78b215eae0d74c7a8ecc
fwatanab <fwatanab@student.42tokyo.jp>
2024-04-26 12:00:44 +0900

    add highFivesGuys()

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -1,12 +1,25 @@
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+FragTrap::FragTrap(const std::string name) : ClapTrap(name) {
 	hitPoint = 100;
 	energyPoint = 100;
 	attackDamage = 30;
-	std::cout << "FragTrap " << name << "Inheritance constructor" << std::endl;
+	std::cout << "FragTrap " << name << " Inheritance constructor" << std::endl;
 }
 
 FragTrap::~FragTrap() {
-	std::cout << "FragTrap " << name << "Inheritance destructor" << std::endl;
+	std::cout << "FragTrap " << name << " Inheritance destructor" << std::endl;
+}
+
+void	FragTrap::attack(const std::string& target) {
+	if (energyPoint > 0) {
+		energyPoint--;
+		std::cout << "FragTrap " << name << " attacks " << target << ", causing " << attackDamage << " points of damage!" << std::endl;
+	}
+	else
+		std::cerr << "FragTrap " << name << " Error: NOT enough energy points" << std::endl;
+}
+
+void	FragTrap::highFivesGuys() {
+	std::cout << "FragTrap " << name <<  "requests high fives guys!" << std::endl;
 }
--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -7,8 +7,9 @@
 
 class	FragTrap : public ClapTrap	{
 	public:
-		FragTrap(std::string name);
+		FragTrap(const std::string name);
 		~FragTrap();
+		void	attack(const std::string& target);
 		void	highFivesGuys(void);
 };
 

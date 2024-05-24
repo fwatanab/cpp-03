a81cc6df5584d53c5ef6d4a4410f0ec17f715b78
jabayaki <jabayaki-system>
2024-05-25 02:11:48 +0900

    Fixed behavior when there is no hitPoint

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -28,25 +28,29 @@ ClapTrap::~ClapTrap() {
 }
 
 void	ClapTrap::attack(const std::string& target) {
-	if (energyPoint > 0) {
+	if (energyPoint > 0 && hitPoint > 0) {
 		energyPoint--;
 		std::cout << "ClapTrap " << name << " attacks " << target << ", causing " << attackDamage << " points of damage!" << std::endl;
 	}
 	else
-		std::cerr << "Error: NOT enough energy points" << std::endl;
+		std::cerr << "ClapTrap " << name << "Error: NOT points" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount) {
-	hitPoint -= amount;
-	std::cout << "ClapTrap " << name << " takes " << amount << " points of damage!" << std::endl;
+	if (hitPoint > 0) {
+		hitPoint -= amount;
+		std::cout << "ClapTrap " << name << " takes " << amount << " points of damage!" << std::endl;
+	}
+	else
+		std::cerr << "ClapTrap " << name << "Error: NOT hit points" << std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount) {
-	if (energyPoint > 0) {
+	if (energyPoint > 0 && hitPoint > 0) {
 		energyPoint--;
 		hitPoint += amount;
 		std::cout << "ClapTrap " << name << " is repaired for " << amount << " points!" << std::endl;
 	}
 	else
-		std::cerr << "ClapTrap " << name << " Error: NOT enough energy points" << std::endl;
+		std::cerr << "ClapTrap " << name << " Error: NOT points" << std::endl;
 }

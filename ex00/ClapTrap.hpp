24f16e06340a05da17c34c6ae176d8a94ad8332e
jabayaki <jabayaki-system>
2024-05-24 17:07:58 +0900

    Orthodox Canonical Form stated otherwise

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -11,7 +11,10 @@ class	ClapTrap {
 		int			energyPoint;
 		int			attackDamage;
 	public:
+		ClapTrap();
 		ClapTrap(const std::string name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
 		~ClapTrap();
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
